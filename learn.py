847950bcb71bdc5089e1a1a646eae1d0adca5b4f
Rohit Bharti <rohitbharti279@gmail.com>
2023-12-07 16:13:47 +0530

    update

--- learn.py
+++ learn.py
@@ -1,2 +1,19 @@
 print("hello world")
 print("hello rohit")
+
+name = "Rohit"
+age = 22
+
+# print(name, age)
+
+# now we can update the variable
+name = "ankit"
+age = 23.04
+
+print(name, age)
+
+is_active = False
+print(is_active + 1)
+
+
+

afd5da5f47f86e3db75f7b33df9eb5532379d1d8
Rohit Bharti <rohitbharti279@gmail.com>
2023-12-07 16:16:53 +0530

    update

--- learn.py
+++ learn.py
@@ -1,19 +1,28 @@
-print("hello world")
-print("hello rohit")
+# print("hello world")
+# print("hello rohit")
 
-name = "Rohit"
-age = 22
+# name = "Rohit"
+# age = 22
+
+# # print(name, age)
+
+# # now we can update the variable
+# name = "ankit"
+# age = 23.04
 
 # print(name, age)
 
-# now we can update the variable
-name = "ankit"
-age = 23.04
-
-print(name, age)
-
-is_active = False
-print(is_active + 1)
+# is_active = False
+# print(is_active + 1)
 
 
+# Assignment assignment
+first_name = "Tony "
+last_name = "Stark"
+age = 51
+is_genius = True
+
+print(first_name + " " + last_name)
+print(first_name + "'s" , age)
+print(is_genius)
 

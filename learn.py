71e0c18c8fe95c21b9416ac8c8d4205506285488
Rohit Bharti <rohitbharti279@gmail.com>
2023-12-08 11:44:32 +0530

    update

--- learn.py
+++ learn.py
@@ -16,13 +16,50 @@
 # print(is_active + 1)
 
 
-# Assignment assignment
-first_name = "Tony "
-last_name = "Stark"
-age = 51
-is_genius = True
+# # Assignment assignment
+# first_name = "Tony "
+# last_name = "Stark"
+# age = 51
+# is_genius = True
 
-print(first_name + " " + last_name)
-print(first_name + "'s" , age)
-print(is_genius)
+# print(first_name + " " + last_name)
+# print(first_name + "'s" , age)
+# print(is_genius)
 
+
+# ALL INPUT STORED AS STRING IN PYTHON
+# all_input = input("what is your name or enter your name:- ")
+# print("hello" + " " + all_input)
+# print(type(all_input))
+
+
+# #type conversion
+# int()
+# float()
+# str()
+# bool()
+
+
+
+# old_age = input("enter your age: ")
+# new_age = int(old_age) + 2
+# print(new_age)
+
+
+
+# x = input("enter :")
+# y = int(x)
+# z = bool(x) + bool(y)
+# print(z)
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+inp = input("enter the any number : ")
+print(type(inp))
+# a = int(inp)
+# print(type(a))
+
+a = float(inp)
+print(type(a))
+print(a)
